a5f5f13904174ed377eed16d86331306ad3258b7
Taichi Uragami <tu.0514.0315@icloud.com>
2021-12-28 17:55:07 +0900

    Initial Commit

---
+++ DocumentPicker/DocumentPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentPickerApp.swift
+//  DocumentPicker
+//
+//  Created by Taichi Uragami on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct DocumentPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
